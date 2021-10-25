979a8119b2363a1639f98e35e66002e765ce7c3a
sehwan08 <sehwan_8@naver.com>
2021-10-25 19:39:53 +0900

    webflux 최종본 (기본세팅/DB세팅 완료)

---
+++ src/main/java/com/cos/fluxdemo/domain/Customer.java
@@ -0,0 +1,16 @@
+package com.cos.fluxdemo.domain;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Data
+public class Customer {
+	
+	@Id
+	private Long id;
+	private final String firstName;
+	private final String lastName;
+}
